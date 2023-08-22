a62db3f85b33fc08b6f155bc8cdf5c84ef8d22c2
dserafinski <d.serafinski@savicki.pl>
2023-08-22 08:42:56 +0200

    WIP

---
+++ config/vimeo.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    */
+
+    'client_id' => env('VIMEO_CLIENT_ID'),
+    'secret' => env('VIMEO_SECRET'),
+    'token' => env('VIMEO_TOKEN'),
+];
